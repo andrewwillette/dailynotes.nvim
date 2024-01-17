4f461efa310ec36d1fe5c597c3378c9070c6a6fb
andrewwillette <willette.andrew1846@gmail.com>
2024-01-17 12:54:11 -0600

    master: daily note shortcut now takes template file

--- lua/dailynotes/init.lua
+++ lua/dailynotes/init.lua
@@ -2,19 +2,50 @@ local M = {}
 M._filetype = nil
 
 M.init = function(filetype)
-    M._filetype = filetype
-    return nil
+  M._filetype = filetype
+  return nil
 end
 
-M.addDailyNoteShortcut = function(keymap, directory)
+M.addDailyNoteShortcut = function(keymap, directory, templateFileURI)
+  local dailyFileString = directory .. "/" .. os.date("%Y-%m-%d") .. M._filetype
+  local dailyFileExists = io.open(dailyFileString, "r")
+  -- if already exists, then just add the keymapping and return
+  if dailyFileExists ~= nil then
     vim.keymap.set(
+      "n",
+      keymap,
+      function()
+        vim.cmd("e " .. dailyFileString)
+      end,
+      {})
+    return nil
+  end
+
+  -- read template file
+  local templateFile, err = io.open(templateFileURI, "rb")
+  if templateFile then
+    local template_content = templateFile:read("*all")
+
+    local dailyFile, err2 = io.open(dailyFileString, "wb")
+    if dailyFile then
+      dailyFile:write(template_content)
+      dailyFile:close()
+      vim.keymap.set(
         "n",
         keymap,
         function()
-            vim.cmd("e " .. directory .. "/" .. os.date("%Y-%m-%d") .. M._filetype)
+          vim.cmd("e " .. dailyFileString)
         end,
         {})
+    else
+      print("error opening dailyNotes new daily file: " .. err2)
+      return nil
+    end
+  else
+    print("error opening dailyNotes template file: " .. err)
     return nil
+  end
+  return nil
 end
 
 return M
--- tests/dailynotes_spec.lua
+++ tests/dailynotes_spec.lua
@@ -1,29 +1,29 @@
 local find_map = function(lhs)
-    local maps = vim.api.nvim_get_keymap('n')
+  local maps = vim.api.nvim_get_keymap('n')
 
-    for _, map in ipairs(maps) do
-        if map.lhs == lhs then
-            return map
-        end
+  for _, map in ipairs(maps) do
+    if map.lhs == lhs then
+      return map
     end
+  end
 end
 
 describe("dailynotes", function()
 
-    it("can be required", function()
-        require("dailynotes")
-    end)
+  it("can be required", function()
+    require("dailynotes")
+  end)
 
-    it("sets the configured keymap correctly", function()
-        local configuredKeyMap = ",t"
-        local configuredDirectory = "/tmp/notes"
-        local configuredFileType = ".txt"
-        local dailynotes = require("dailynotes")
-        dailynotes.init(configuredFileType)
-        assert.are.same(dailynotes._filetype, configuredFileType)
-        dailynotes.addDailyNoteShortcut(configuredKeyMap, configuredDirectory)
-        local found = find_map(configuredKeyMap)
-        assert.equals(found.lhs, configuredKeyMap)
-        assert.truthy(found.lhs ~= nil)
-    end)
+  it("sets the configured keymap correctly", function()
+    local configuredKeyMap = ",t"
+    local configuredDirectory = "~/tmp/notes"
+    local configuredFileType = ".txt"
+    local dailynotes = require("dailynotes")
+    dailynotes.init(configuredFileType)
+    assert.are.same(dailynotes._filetype, configuredFileType)
+    dailynotes.addDailyNoteShortcut(configuredKeyMap, configuredDirectory, "/Users/rjm995/tmp/dailyCapitalOneNotesTemplate.md")
+    local found = find_map(configuredKeyMap)
+    assert.equals(found.lhs, configuredKeyMap)
+    assert.truthy(found.lhs ~= nil)
+  end)
 end)
